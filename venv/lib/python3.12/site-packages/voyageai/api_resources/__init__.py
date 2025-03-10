2afff9be8ac151bd50f0bbdf69cde3c8656555e2
michelleroydelarredeladorie <132815508+michelleroydelarredeladorie@users.noreply.github.com>
2025-03-10 15:22:29 -0400

    Delete venv directory

--- venv/lib/python3.12/site-packages/voyageai/api_resources/__init__.py
+++
@@ -1,7 +0,0 @@
-from voyageai.api_resources.response import VoyageResponse
-from voyageai.api_resources.api_requestor import VoyageHttpResponse
-
-from voyageai.api_resources.api_resource import APIResource
-from voyageai.api_resources.embedding import Embedding
-from voyageai.api_resources.reranking import Reranking
-from voyageai.api_resources.multimodal_embedding import MultimodalEmbedding
